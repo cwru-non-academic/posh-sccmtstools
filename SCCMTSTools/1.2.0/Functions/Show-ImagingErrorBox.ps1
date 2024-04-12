b05f0e3a40f26d80366bba5a6fe8118fb1f0ae3c
Dan Thompson <danthompson@case.edu>
2024-04-12 10:59:12 -0400

    Added Show-ImagingErrorBox function.

--- SCCMTSTools/1.2.0/Functions/Show-ImagingErrorBox.ps1
+++ SCCMTSTools/1.2.0/Functions/Show-ImagingErrorBox.ps1
@@ -1,3 +1,40 @@
 function Show-ImagingErrorBox {
+    <#
+        .SYNOPSIS
+            Shows a dialog box used to let the user know that imaging the computer failed.
+        .DESCRIPTION
+            Shows a dialog box used to let the user know that imaging the computer failed.
 
+            This includes:
+                * A message indicating the task sequence failed.
+                * The step the task sequence failed on.
+                * The error code.
+            
+            This must be called from an interactive PowerShell session. ServiceUI can do this.
+
+        .NOTES
+            Author    : Dan Thompson
+            Copyright : 2024 Case Western Reserve University
+    #>
+
+    [CmdletBinding()]
+
+    param(
+        # An additional message to display, such as asking the technician doing to image to check your
+        # knowledgebase. This is optional.
+        [Parameter()]
+        [string]$ExtraMessage
+    )
+
+    # Show the dialog box.
+
+    $Body = "The imaging process failed.`n"
+    $Body += "`tFailing Step: $(Get-TSVariable -Name '_SMSTSLastActionName')`n"
+    $Body += "`tError Code: $(Get-TSVariable -Name '_SMSTSLastActionRetCode')"
+
+    if ($PSBoundParameters.ContainsKey('ExtraMessage')) {
+        $Body += "`n$ExtraMessage"
+    }
+
+    Show-MessageBox -Body $Body -Title 'Imaging Error' -Image 'Error'
 }
